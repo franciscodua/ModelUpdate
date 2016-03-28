674be402135ffe1d9a8a6bdd53e282a4f29bba94
blitzbot <franciscodua@gmail.com>
2016-03-28 14:38:46 +0100

    Fix: test.py path regarding test.sh

--- New Rules/test.sh
+++ New Rules/test.sh
@@ -3,5 +3,5 @@
 for i in `seq 1 8`;
 do
 	echo "Test: $i"
-	python test.py ~/Documents/IST/Presentation/Testes/KMeans/test$i.csv Plots/test$i
+	python Visualization/test.py ~/Documents/IST/Presentation/Testes/KMeans/test$i.csv Plots/test$i
 done
